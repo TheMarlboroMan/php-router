b41bf8c4d111fa81dc69e2bb875b5432c127b9c0
Daniel Pastor <dpastor@comvive.es>
2021-11-30 09:27:05 +0100

    WIP, only the app implementation should be left... and testing, of course

--- example/src/app/controller/contacts.php
+++ example/src/app/controller/contacts.php
@@ -8,7 +8,33 @@ class contacts extends controller {
 		?string $_filter_name
 	) :\srouter\controller_response {
 
-		//TODO:
+		$rolodex=$this->dc->get_rolodex();
+
+		$contacts=array_filter(
+			$rolodex->get_all(),
+			function(\app\rolodex_contact $_node) use ($_filter_name, $_filter_number) {
+
+				$name_ok=true;
+				if(null !== $_filter_name) {
+
+					$name_ok=false!==strpos($_node->get_name(), $_filter_number);
+				}
+
+				$number_ok=true;
+				if(null !== $_filter_number) {
+
+					$number_ok=false!==strpos($_node->get_number(), $_filter_number);
+				}
+
+				return $name_ok && $number_ok;
+			}
+		);
+
+		return new \srouter\controller_response(
+			200,
+			[],
+			$contacts
+		);
 	}
 
 	public function post(
@@ -17,14 +43,41 @@ class contacts extends controller {
 		string $_company
 	) :\srouter\controller_response {
 
-		//TODO:
+		$rolodex=$this->dc->get_rolodex();
+
+		$entry=new \app\rolodex_contact();
+		$entry->set_name($_name)
+			->set_phone($_phone)
+			->set_company($_company);
+
+		$entry=$rolodex->create($entry);
+
+		return new \srouter\controller_response(
+			201,
+			[
+				new \srouter\http_response_header("location", "contacts/".$entry->get_id())
+			],
+			"created"
+		);
 	}
 
 	public function info(
 		int $_id
 	) :\srouter\controller_response {
 
-		//TODO:
+		$rolodex=$this->dc->get_rolodex();
+		$entry=$rolodex->find_by_id($_id);
+
+		if(null===$entry) {
+
+			return new \srouter\controller_response(404, [], "not found");
+		};
+
+		return new \srouter\controller_response(
+			200,
+			[],
+			$entry
+		);
 	}
 
 	public function patch(
@@ -34,14 +87,41 @@ class contacts extends controller {
 		?string $_company
 	) :\srouter\controller_response {
 
-		//TODO:
+		$rolodex=$this->dc->get_rolodex();
+		$entry=$rolodex->find_by_id($_id);
+
+		if(null!==$_name) {
+
+			$entry->set_name($_name);
+		}
+
+		if(null!==$_phone) {
+
+			$entry->set_phone($_phone);
+		}
+
+		if(null!==$_company) {
+
+			$entry->set_company($_company);
+		}
+
+		$rolodex->patch($entry);
+		return new \srouter\controller_response(200, [], "patched");
 	}
 
 	public function delete(
-		int $_id,
+		int $_id
 	) :\srouter\controller_response {
 
-		//TODO:
-	}
+		$rolodex=$this->dc->get_rolodex();
+		$entry=$rolodex->find_by_id($_id);
 
+		if(null===$entry) {
+
+			return new \srouter\controller_response(404, [], "not found");
+		};
+
+		$rolodex->delete($entry);
+		return new \srouter\controller_response(200, [], "deleted");
+	}
 }
